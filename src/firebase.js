713aaaad19e8fcdadafcad65aff1b1d175b91186
Djamel DADA <44305742+dadadjamel@users.noreply.github.com>
2021-01-06 11:40:18 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,7 @@
 import firebase from "firebase"
 
 const firebaseApp = firebase.initializeApp({
-    apiKey: "AIzaSyAOJfB7MaEvUV9mCh6ijn2fS23xCaO_A00",
-    authDomain: "instagram-clone-3d976.firebaseapp.com",
-    databaseURL: "https://instagram-clone-3d976.firebaseio.com",
-    projectId: "instagram-clone-3d976",
-    storageBucket: "instagram-clone-3d976.appspot.com",
-    messagingSenderId: "602580428237",
-    appId: "1:602580428237:web:cc7d1d89d1f891eaf69084",
-    measurementId: "G-6YDYGDDLYG"
+    
 })
 
 const db = firebaseApp.firestore()
